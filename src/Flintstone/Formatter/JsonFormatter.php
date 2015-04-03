196bf9586f0370e6da8fc7137d4560da6314fc9c
Joshua Johnson <joshua@lylejohnson.us>
2015-04-03 00:18:27 -0400

    1) Updated JsonFormatter to throw an Exception if the data being encoded was an associative array. I did this because it is impossible to store an associative array as a JSON object. 2) Updated unit tests to reflect changes allowing us to store objects. 3) If caching is turned off and you do an assertSame on an object in the database, you will have two different references to the same object which means you will fail assertion. I changed several assertSame to assertEquals to get past this issue of looking at reference objects.

--- src/Flintstone/Formatter/JsonFormatter.php
+++ src/Flintstone/Formatter/JsonFormatter.php
@@ -7,24 +7,39 @@
 
 namespace Flintstone\Formatter;
 
+use Flintstone\FlintstoneException;
+
 /**
  * Encodes/decodes data into JSON
  */
-class JsonFormatter implements FormatterInterface
-{
+class JsonFormatter implements FormatterInterface {
+
     /**
      * {@inheritdoc}
      */
-    public function encode($data)
-    {
+    public function encode($data) {
+        if (isset($data) && is_array($data) && $this->isAssocativeArray($data)) {
+            throw new FlintstoneException('Associate arrays cannot be stored as JSON Values');
+        }
+
         return json_encode($data);
     }
 
     /**
      * {@inheritdoc}
      */
-    public function decode($data)
-    {
+    public function decode($data) {
         return json_decode($data);
     }
+
+    /**
+     * Determines if passed in array is an associative array.
+     * 
+     * @param array $arr Array you'd like to check
+     * @return boolean If $arr is an associative array
+     */
+    private function isAssocativeArray($arr) {
+        return array_keys($arr) !== range(0, count($arr) - 1);
+    }
+
 }
